755e68d375b3101a01ef052a2436a5e653d8616c
Kazuma-Iki <k_iki@echo.nuee.nagoya-u.ac.jp>
2014-02-27 13:22:00 +0900

    2nd commit

---
+++ no1/no1b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+
+	char retsu1[256];
+	char retsu2[256];
+	int mojisu1;
+	int mojisu2;
+	int i;
+	int j;
+
+	strcpy(retsu1,"KAZUMAIKI"); mojisu1=9;
+	strcpy(retsu2,"UMAI");      mojisu2=4;
+
+	for(i=0;i<mojisu1-(mojisu2-1);i++){
+		if(mojiretsu1[i]=mojiretsu2[0]){
+		}
+	}
+
+
+	printf("false\n");
+
+	return 0;
+}
